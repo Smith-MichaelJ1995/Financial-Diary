1125235d1e395afa70ff9331fcf4daedfffeb6bd
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2019-08-13 20:50:42 -0400

    Fixed syntax error breaking the web UI and mobile app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 // Import Child Components
 import Test from './components/Test';
 
-function App() {
+export default function App() {
 
-  return <Test />;
+  return (
+    <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! 111</Text>
+    </View>
+  );
+
+
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
